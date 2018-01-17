bf4339c2455146d64ec8df84977c00862275a704
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2018-01-17 01:22:35 -0600

    All the code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # flask-from-scratch
+
+This is the code of the tutorial Python Flask From Scratch by Traversy Media
+
+https://www.youtube.com/watch?v=zRwy8gtgJ1A&list=PLillGF-RfqbbbPz6GSEM9hLQObuQjNoj_
---
+++ myflaskapp/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body' : 'This is a body',
+            'author' : 'Carlos Arroyo',
+            'create_date': '12-01-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body' : 'This is a head',
+            'author' : 'Antonio Villalobos',
+            'create_date': '11-01-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body' : 'About brain',
+            'author' : 'Nathalie Portman',
+            'create_date': '22-05-2007'
+        }
+    ]
+    return articles
